f751b3fff06d3b76bbdd49a8ab370b9e1701d62a
Sarah Sooklall <sarah.sooklall@f1006-04-34124.bmcc.cuny.edu>
2018-02-21 17:59:58 -0500

    Re-added the content that Xiuling did because it was not added to git

---
+++ app.js
@@ -0,0 +1,17 @@
+const yellowButton = document.getElementById("yellow");
+const blueButton = document.getElementById("blue");
+const greenButton = document.getElementById("green");
+const redButton = document.getElementById("red");
+const orangeButton = document.getElementById("orange");
+const purpleButton = document.getElementById("purple");
+const pinkButton = document.getElementById("pink");
+const brownButton = document.getElementById("brown");
+const whiteButton = document.getElementById("white");
+const blackButton = document.getElementById("black");
+
+yellowButton.onclick = function() {
+    const message = document.createElement("p");
+    message.textContent = yellow.value
+                        + "Yellow is the color of sunshine. It is associated with joy, happiness, intellect, and energy."
+    document.body.appendChild(message);
+}
